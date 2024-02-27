877d9ed731519d3dfd47052af05fa31b876c6ca1
Evgeniy.Lychkovsky <cdsa1995@mail.ru>
2024-02-27 17:03:21 +0300

    Added new fextures. Need to solve problem with getInfoAboutHero

--- src/main/java/org/example/heroes/Abaddon.java
+++ src/main/java/org/example/heroes/Abaddon.java
@@ -11,11 +11,12 @@ import java.util.List;
 
 public class Abaddon {
 
-    private static final String URLHero = "https://www.dotabuff.com/heroes/abaddon";
-    private static final String URLHeroCounters = "https://www.dotabuff.com/heroes/abaddon/counters/";
+    private static final String URLHeroDB = "https://www.dotabuff.com/heroes/abaddon";
+    private static final String URLHeroDBCounters = "https://www.dotabuff.com/heroes/abaddon/counters/";
+    private static final String URLHeroStratz = "https://stratz.com/heroes/102-abaddon";
 
     public static void getInfoAboutAbaddon() throws IOException {
-        Document doc = Jsoup.connect(URLHero).get();
+        Document doc = Jsoup.connect(URLHeroDB).get();
 
         for (Element table : doc.select("table.other")) {
             for (Element row : table.select("td")) {
@@ -24,8 +25,35 @@ public class Abaddon {
         }
     }
 
+    public static void getInfoAboutAbaddonLanes() throws IOException {
+        Document doc = Jsoup.connect(URLHeroStratz).get();
+
+        for (Element table : doc.select("div.hitagi__sc-1ah81hi-0.hitagi__sc-ucbcwr-0.ibjsOm.hitagi__sc-ymfzjp-0.hitagi__sc-ymfzjp-1.cNgA-Dx")) {
+            for (Element row : table.select("span.hitagi__sc-6oal1n-0.jRxcof")) {
+                System.out.println(row.select("span").text());
+            }
+            for (Element row : table.select("div.hitagi__sc-1ah81hi-0.hUBfhn")) {
+                for (Element value : row.select("div.hitagi__sc-1ah81hi-0.dWldeB")) {
+                    System.out.println("Win Rate: " + (value.select("div").text()));
+                }
+            }
+            for (Element row : table.select("div.hitagi__sc-1ah81hi-0.gkLxSF")) {
+                for (Element value : row.select("div.hitagi__sc-ymfzjp-0.hitagi__sc-ymfzjp-1.iElLgn")) {
+                    System.out.println("Laning: " + (value.select("div").text()));
+                }
+            }
+//            for (Element row : table.select("div.hitagi__sc-1ah81hi-0.gkLxSF")) {
+//                for (Element test : row.select("div.hitagi__sc-ymfzjp-0.hitagi__sc-ymfzjp-1.kDpEVc")) {
+//                    if (test.className().equals("hitagi__sc-1apuy4g-0.ketdco")) {
+//                        System.out.println(test.select("div").text());
+//                    }
+//                }
+//            }
+        }
+    }
+
     public static void getInfoAboutAbaddonIsCounteredBy() throws IOException {
-        Document doc = Jsoup.connect(URLHeroCounters).get();
+        Document doc = Jsoup.connect(URLHeroDBCounters).get();
 
         List<String> list = new ArrayList<>();
 
@@ -41,7 +69,7 @@ public class Abaddon {
     }
 
     public static void getInfoAboutAbaddonCounters() throws IOException {
-        Document doc = Jsoup.connect(URLHeroCounters).get();
+        Document doc = Jsoup.connect(URLHeroDBCounters).get();
 
         List<String> list = new ArrayList<>();
 

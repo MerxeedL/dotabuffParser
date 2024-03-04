175a5b1f712e0c20fa96ea7528304262e0b1aca5
Evgeniy.Lychkovsky <cdsa1995@mail.ru>
2024-03-04 12:41:39 +0300

    Added method ChangeLogs Items.

--- src/main/java/org/example/items/mainitems/attributesItems/BeltOfStrength.java
+++ src/main/java/org/example/items/mainitems/attributesItems/BeltOfStrength.java
@@ -1,4 +1,30 @@
 package org.example.items.mainitems.attributesItems;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class BeltOfStrength {
+    private static final String URL = "https://dota2.fandom.com/ru/wiki/Belt_of_Strength";
+    public static void getInfoAboutChangelogs() throws IOException {
+
+        Document doc = Jsoup.connect(URL).get();
+
+        Elements entries = doc.select(".updatetablebody #entry");
+
+        for (Element entry: entries) {
+            Element version = entry.selectFirst("#version");
+            String versionText = version.text();
+
+            Element description = entry.selectFirst("#description");
+            String descriptionText = description.text();
+
+            System.out.println("Version: " + versionText);
+            System.out.println("Description: " + descriptionText);
+        }
+    }
+
 }
--- src/main/java/org/example/items/mainitems/attributesItems/IronBranch.java
+++ src/main/java/org/example/items/mainitems/attributesItems/IronBranch.java
@@ -1,4 +1,30 @@
 package org.example.items.mainitems.attributesItems;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class IronBranch {
+    private static final String URL = "https://dota2.fandom.com/ru/wiki/Iron_Branch";
+    public static void getInfoAboutChangelogs() throws IOException {
+
+        Document doc = Jsoup.connect(URL).get();
+
+        Elements entries = doc.select(".updatetablebody #entry");
+
+        for (Element entry: entries) {
+            Element version = entry.selectFirst("#version");
+            String versionText = version.text();
+
+            Element description = entry.selectFirst("#description");
+            String descriptionText = description.text();
+
+            System.out.println("Version: " + versionText);
+            System.out.println("Description: " + descriptionText);
+        }
+    }
+
 }
